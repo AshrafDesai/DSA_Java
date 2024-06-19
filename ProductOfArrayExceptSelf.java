51f6b195c77c7d5c0d9d897beb9141c76a2e19ae
Asharafraza Desai <asharafrazadesai2002@gmail.com>
2024-06-19 15:51:16 +0530

    medium

--- ProductOfArrayExceptSelf.java
+++ ProductOfArrayExceptSelf.java
@@ -2,28 +2,30 @@ import java.util.Arrays;
 
 public class ProductOfArrayExceptSelf {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4};
-        int[] result = productExceptSelf(nums);
+        int[] arr = {1, 2, 3, 4}; 
+        int[] result = product(arr);
         System.out.println("Product of array except self: " + Arrays.toString(result));
     }
 
-    public static int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        
-       
-        result[0] = 1;
-        for (int i = 1; i < n; i++) {
-            result[i] = result[i - 1] * nums[i - 1];
+    public static int[] product(int[] arr) {
+        int[] left = new int[arr.length];
+        int[] right = new int[arr.length];
+        int[] ans = new int[arr.length];
+
+        left[0] = 1;
+        for (int i = 1; i < arr.length; i++) {
+            left[i] = left[i - 1] * arr[i - 1];
         }
-        
-        
-        int rightProduct = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            result[i] *= rightProduct;
-            rightProduct *= nums[i];
+
+        right[arr.length - 1] = 1;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * arr[i + 1];
         }
-        
-        return result;
+
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = left[i] * right[i];
+        }
+
+        return ans;
     }
 }
