2c2184cf0a5924c1258b100dd4258d93d791d151
Asharafraza Desai <asharafrazadesai2002@gmail.com>
2024-06-27 16:20:06 +0530

    Stack

--- Stack/Largestrectanglehistogram.java
+++ Stack/Largestrectanglehistogram.java
@@ -1,5 +1,31 @@
 package Stack;
 
-public class Largestrectanglehistogram {
-    
+import java.util.Stack;
+public class Largestrectanglehistogram 
+{
+
+    public static void main(String[] args) 
+    {
+        int[] heights = {2, 1, 5, 6, 2, 3};
+        System.out.println("Largest rectangle area: " + largestRectangleArea(heights));
+    }
+    public static int largestRectangleArea(int[] heights) 
+    {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+        for (int i = 0; i <= n; i++) 
+        {
+            int h = (i == n ? 0 : heights[i]);
+            while (!stack.isEmpty() && heights[stack.peek()] > h) 
+            {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
 }
--- Stack/reverseString.java
+++ Stack/reverseString.java
@@ -1,21 +1,25 @@
 package Stack;
+
 import java.util.Stack;
-public class reverseString {
-    public static void main(String[] args)
+
+public class reverseString 
+{
+    public static void main(String[] args) 
     {
         String input = "Hello World";
-        String reversed = reverseString(input);
+        String reversed = reverse(input);
         System.out.println(reversed);
     }
-    public static String reverseString(String s)
+
+    public static String reverse(String s) 
     {
         Stack<Character> stack = new Stack<>();
-        for(int i=0;i<s.length();i++)
+        for (int i = 0; i < s.length(); i++) 
         {
             stack.push(s.charAt(i));
         }
         StringBuilder reversed = new StringBuilder();
-        while(!stack.isEmpty())
+        while (!stack.isEmpty()) 
         {
             reversed.append(stack.pop());
         }
