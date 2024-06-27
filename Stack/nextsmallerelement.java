c0bb2e870f0177783706772e46d957943e9f5483
Asharafraza Desai <asharafrazadesai2002@gmail.com>
2024-06-27 16:10:07 +0530

    Stack

--- Stack/nextsmallerelement.java
+++ Stack/nextsmallerelement.java
@@ -9,9 +9,11 @@ public class nextsmallerelement {
         int[] result = new int[n];
         Stack<Integer> stack = new Stack<>();
         stack.push(-1);
-        for (int i = n - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) 
+        {
             int current = arr[i];
-            while (stack.peek() != -1 && stack.peek() >= current) {
+            while (stack.peek() != -1 && stack.peek() >= current) 
+            {
                 stack.pop();
             }
             result[i] = stack.peek();
@@ -19,18 +21,21 @@ public class nextsmallerelement {
         }
         return result;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         nextsmallerelement nse = new nextsmallerelement();
         int[] arr = {4, 8, 5, 2, 25};
         int[] result = nse.nextSmall(arr);
 
         System.out.println("Original array: ");
-        for (int num : arr) {
+        for (int num : arr) 
+        {
             System.out.print(num + " ");
         }
 
         System.out.println("\nNext smaller elements: ");
-        for (int num : result) {
+        for (int num : result) 
+        {
             System.out.print(num + " ");
         }
     }
