0268effcd606b6bd859512c641c9bad56b188b46
Asharafraza Desai <asharafrazadesai2002@gmail.com>
2024-07-09 17:20:06 +0530

    Binary_Search_basedproblem

---
+++ Search/first_last_position_of_element.java
@@ -0,0 +1,66 @@
+package Search;
+
+public class first_last_position_of_element {
+    
+    public static int firstOcc(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1; 
+        int ans = -1;
+
+        while (left <= right) 
+        {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == key) 
+            {
+                ans = mid;
+                right = mid - 1;
+            } 
+            else if (key > arr[mid]) 
+            {
+                left = mid + 1;
+            } 
+            else 
+            {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int lastOcc(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1; 
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == key) 
+            {
+                ans = mid;
+                left = mid + 1; 
+            } 
+            else if (key > arr[mid]) 
+            {
+                left = mid + 1;
+            } 
+            else 
+            {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 3, 5 };
+        int key = 3;
+
+        int firstIndex = firstOcc(array, key);
+        int lastIndex = lastOcc(array, key);
+
+        System.out.println("First Occurrence of " + key + ": " + firstIndex);
+        System.out.println("Last Occurrence of " + key + ": " + lastIndex);
+    }
+}
